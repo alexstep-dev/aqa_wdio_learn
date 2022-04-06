7a2629e2658eac8cf03feef229b2775c19121a13
Oleksandr Stepanenko <ostepanenko@intellias.com>
2022-04-06 08:35:45 +0300

    comment

--- test/specs/upload.js
+++ test/specs/upload.js
@@ -1,11 +1,11 @@
 const appURL = "http://localhost:3000";
 
 describe("Upload", function () {
-
   it("Iframe", async function () {
     await browser.url(appURL);
     await browser.switchToFrame(await $("#frame"));
 
+    // eslint (wdio/await-expect) should emmit an error here
     expect($("#player")).toExist();
   });
 });
